7aab4bf0a2ed0c15d3cb20949c8cdc3a350a2bd5
shree675 <shreeteshm37@gmail.com>
2021-02-11 00:04:56 +0530

    blah

--- client-gallery/src/components/Banner/index.js
+++ client-gallery/src/components/Banner/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Jumbotron } from 'react-bootstrap';
 // import styled from 'styled-components';
 
 const HeroBanner = (props) => {
 
     const linksList=props.links;
-    // const numberOfImages=linksList.length;
+    const numberOfImages=linksList.length;
     const url=linksList[0];
     const pagename=props.pagename;
+    // const [j,setIndex] = useState(0);
+    var j=-1;
+
+    const [cururl,setCururl] = useState(url);
+
+    setInterval(()=>{
+        j=(j+1)%numberOfImages;
+        setCururl(linksList[j]);
+    },3000);
 
     return (
         <div className="j-shr">
@@ -21,7 +30,7 @@ const HeroBanner = (props) => {
                 rgba(0, 0, 0, 0.45), 
                 rgba(0, 0, 0, 0.45)
                 ),
-                url(${url})`}}>
+                url(${cururl})`}}>
                 
             </Jumbotron>
 
